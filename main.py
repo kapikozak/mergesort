9def8f1fb2af5f5aa89fe55d2cf84cd3c3e9c48e
kapikozak <kapikozak@gmail.com>
2026-04-08 20:18:10 +0200

    Readability improvements.

--- main.py
+++ main.py
@@ -1,48 +1,30 @@
-import logging
 from typing import List
 
-logging.basicConfig(level=logging.INFO)
+def merge_sort(arr: List[int]) -> None:
+    n = len(arr)
+    dummy = [0] * n
+    i = 1
+    while i < n:
+        for low in range(0, n, 2 * i):
+            mid, high = low + i - 1, min(low + 2 * i - 1, n - 1)
+            j, k = low, mid + 1
+            for l in range(low, high + 1):
+                if j > mid:
+                    dummy[l] = arr[k]
+                    k += 1
+                elif k > high:
+                    dummy[l] = arr[j]
+                    j += 1
+                else:
+                    if arr[j] <= arr[k]:
+                        dummy[l] = arr[j]
+                        j += 1
+                    else:
+                        dummy[l] = arr[k]
+                        k += 1
 
-def merge_sort(a: List[int], f: int|None = None, l: int|None = None) -> None:
-    def merge(f_start: int, f_end: int, s_start: int, s_end: int) -> None:
-        i, j, k = f_start, s_start, f_start
-        while i <= f_end and j <= s_end:
-            if a[i] <= a[j]:
-                b[k] = a[i]
-                i += 1
-            else:
-                b[k] = a[j]
-                j += 1
-            k += 1
-
-        if i <= f_end:
-            b[k:s_end + 1] = a[i:f_end + 1]
-        elif j <= s_end:
-            b[k:s_end + 1] = a[j:s_end + 1]
-
-        a[f_start:s_end + 1] = b[f_start:s_end + 1]
-
-    if f is None:
-        logging.info('f argument was not specified, code runs with 0')
-        f = 0
-    if l is None:
-        logging.info(f'l argument was not specified, code runs with {len(a)-1}')
-        l = len(a) - 1
-
-    n = l - f + 1
-    b = [0] * len(a)
-    size = 1
-    while size < n:
-        first = f
-        while first <= l - size:
-            first_end = first + size - 1
-            second = first_end + 1
-            second_end = min(second + size - 1, l)
-            merge(first, first_end, second, second_end)
-            first += 2 * size
-        size <<= 1
-#bottom up merge sort
-
+            arr[low:high + 1] = dummy[low:high + 1]
+        i <<= 1
 
 if __name__ == '__main__':
     with open('list.txt', 'r') as f:
